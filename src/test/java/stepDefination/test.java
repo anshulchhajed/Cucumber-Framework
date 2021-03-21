1dffb2e59c0b0d1eecfc919d75ad05c6581f94a5
anshulchhajed <anshulchhajed@localhost>
2021-03-21 14:38:38 +0530

    new

---
+++ src/test/java/stepDefination/test.java
@@ -0,0 +1,8 @@
+package stepDefination;
+
+public class test {
+public static void main(String[] args) {
+	 String currentDir = System.getProperty("user.dir");
+	 System.out.println(currentDir);
+}
+}
